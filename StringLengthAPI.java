5534d4082e8bb3507987397f6243e81d5bceaf9e
Waterwolf9910 <46389474+Waterwolf9910@users.noreply.github.com>
2019-11-06 16:23:22 +0000

    Update StringLengthAPI.java

--- StringLengthAPI.java
+++ StringLengthAPI.java
@@ -1,7 +1,13 @@
 package Waterwolf9910ScienceProject2019;
 
+import java.util.*;
+import java.time.*;
+
 public class StringLengthAPI {
+     Scanner input = new Scanner(System.in);
      public int length() {
-           return value.length;
-       }
+          System.out.print("How many character are in the string (including strings): ");
+          int length = input.nextInt();
+          return value.length;
+     }
 }
