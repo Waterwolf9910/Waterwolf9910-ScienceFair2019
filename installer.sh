49b66d8ca047964774532ffae6b18b544c82c4ae
Waterwolf9910 <46389474+Waterwolf9910@users.noreply.github.com>
2019-11-18 13:38:04 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ sleep 5s
 mkdir Caeser_Cipher_encrypt-decrypt
 cd Caeser_Cipher_encrypt-decrypt/
 git clone -b master https://github.com/Waterwolf9910/Waterwolf9910-ScienceFair2019/
-cd Caeser_Cipher_encrypt-decrypt/Waterwolf9910-ScienceFair2019
+cd Waterwolf9910-ScienceFair2019
 javac CaesarCipher.java
 cd ..
 cd ..
