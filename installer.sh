d5705e68b64a3a8398f51b2a7aca90777685079e
Waterwolf9910 <46389474+Waterwolf9910@users.noreply.github.com>
2019-11-25 08:50:15 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,7 +14,7 @@ sudo apt install git openjdk-8-jdk openjdk-8-jre
 sleep 5s
 mkdir Caesar_Cipher_encrypt-decrypt
 cd Caesar_Cipher_encrypt-decrypt/
-git clone -b master https://github.com/Waterwolf9910/Waterwolf9910-ScienceFair2019/
+git clone https://github.com/Waterwolf9910/Waterwolf9910-ScienceFair2019/
 cd Waterwolf9910-ScienceFair2019
 javac CaesarCipher.java
 chmod +x run.sh
