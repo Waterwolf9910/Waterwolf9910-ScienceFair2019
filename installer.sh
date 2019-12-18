832f992916adb0707c06b2d76f76e268f60f7a87
Waterwolf9910 <46389474+Waterwolf9910@users.noreply.github.com>
2019-12-18 14:06:08 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo Made by Waterwolf9910 and Andon1379
+echo 
+echo 
+echo 
+echo Installing
+function pause() {
+     read -p "$*"
+}
+mv installer.sh ~
+cd ~
+sudo apt install git openjdk-8-jdk openjdk-8-jre
+sleep 5s
+mkdir Caesar_Cipher_encrypt-decrypt
+cd Caesar_Cipher_encrypt-decrypt/
+git clone https://github.com/Waterwolf9910/Waterwolf9910-ScienceFair2019/
+cd Waterwolf9910-ScienceFair2019
+javac CaesarCipher.java
+chmod +x run.sh
+cd ~
+mv installer.sh Caesar_Cipher_encrypt-decrypt/Waterwolf9910-ScienceFair2019/
+echo run run.sh
+pause 'Press [enter] to end script'
+exit 0
