72344e501dc3c33595ad06749e88366d34911c70
Waterwolf9910 <46389474+Waterwolf9910@users.noreply.github.com>
2020-01-10 06:49:02 -0500

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -6,23 +6,20 @@ echo .
 echo .
 echo enjoy!
 
-setlocal
-set /p run-install="run or install: "
-if %run-install% == install goto installer
-if %run-install% == run goto runner
-:installer
-@echo on
+
+cd %homepath%
 mkdir Ceasar_Cipher_encrypt-decrypt
 cd Ceasar_Cipher_encrypt-decrypt\
 git clone -b master https://github.com/Waterwolf9910/Waterwolf9910-ScienceFair2019/
-cd Ceaser_Cipher_encrypt-decrypt\Waterwolf9910-ScienceFair2019
+cd Waterwolf9910-ScienceFair2019
+rd /s /q .github
+rd /s /q .git
+erase .gitignore
+erase installer.bat
+erase uninsall.sh
+move * ..\
+cd ..
 javac CeasarCipher.java
-endlocal
-set run-install=
 pause
 exit 0
 
-:runner
-java CeasarCipher
-pause
-exit 0
